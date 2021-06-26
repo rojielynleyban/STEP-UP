f93290393ad377f8fa2139e25fc51bf698928f56
rojielynleyban <58040756+rojielynleyban@users.noreply.github.com>
2021-06-26 11:32:06 +0800

    STEP UP - StepUp script

--- Assets/Scripts/Level1CompletedNextLevel.cs
+++ Assets/Scripts/Level1CompletedNextLevel.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class Level1CompletedNextLevel : MonoBehaviour { 
 
     public void PlayGame()
-{
+    {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-}
+    }
 }
---
+++ Assets/Scripts/STEP UP.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class STEPUP : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PauseGame()
+    {
+        PlayerPrefs.SetInt("SavedScene", SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(41);
+    }
+
+    //Exit or X icon in FinalGame
+    public void QuitApplication()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    // X icon
+    public void QuitGame()
+    {
+        PlayerPrefs.SetInt("SavedScene", SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(0);
+    }
+
+    public void MainMenu()
+    {
+        PlayerPrefs.SetInt("SavedScene", SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(0);
+    }
+
+    //Restart or TryAgain
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //Continue or Resume
+    public void ResumeGame()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("SavedScene"));
+    }
+
+    //Settings or AboutUs
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene(38);
+    }
+
+    public void AboutUs()
+    {
+        SceneManager.LoadScene(37);
+    }
+
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene(40);
+    }
+
+    public void Sound()
+    {
+        SceneManager.LoadScene(39);
+    }
+
+    public void SoundBack()
+    {
+        SceneManager.LoadScene(38);
+    }
+}
